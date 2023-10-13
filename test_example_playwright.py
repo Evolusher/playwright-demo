8517662d56f763743cbb6aa27da65934e50612fb
Evolusher <andrii.bilenkyi@gmail.com>
2023-10-13 15:17:17 +0100

    comments adjustment

--- test_example_playwright.py
+++ test_example_playwright.py
@@ -4,10 +4,10 @@ from playwright.sync_api import Page, expect
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
 
-    # Click the get started link.
+    # Switch to Pythion docs.
     page.get_by_role("button", name = "Node.js").hover()
     page.get_by_label("Main", exact=True).get_by_role("link", name="Python").click()
 
 
-    # Expects page to have a heading with the name of Installation.
+    # Expects page to have a URL with path of python.
     expect(page).to_have_url(re.compile("python"))
